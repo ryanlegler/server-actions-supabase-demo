1d16dd8399ecebca671942582e7015f536bf1690
Ryan Legler <ryan.legler@gmail.com>
2023-05-06 10:11:33 -0700

    enhancement: mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,11 +41,13 @@ function User({ user }: any) {
                         height={100}
                     />
                 ) : null}
-                <div className="flex flex-col items-center justify-center">
-                    <span className="text-xl font-bold text-center">
+                <div className="flex flex-col items-center justify-center w-full">
+                    <span className="sm:text-xl text-m font-bold text-center">
                         {user.firstName} {user.lastName}
                     </span>
-                    <span className="text-l">{user.email}</span>
+                    <span className="sm:text-l text-xs text-ellipsis w-full overflow-hidden">
+                        {user.email}
+                    </span>
                 </div>
             </div>
         </div>
@@ -63,9 +65,9 @@ export default async function Home() {
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
-            <div className="grid grid-cols-5 gap-4 items-start">
-                <div className="flex col-span-1 justify-center flex-col gap-4">
+        <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-6 bg-black text-white">
+            <div className="grid grid-cols-2 sm:grid-cols-5 gap-4 items-start">
+                <div className="flex sm:col-span-1 col-span-1 justify-center flex-col gap-4">
                     <User user={user} />
 
                     {/* <form action={onAction}>
@@ -75,7 +77,7 @@ export default async function Home() {
 
                     <Button onClick={onAction} />
                 </div>
-                <div className="col-span-4 grid grid-cols-4 gap-4 ">
+                <div className="sm:col-span-4 col-span-1 grid sm:grid-cols-4 grid-cols-1 gap-4 ">
                     {users?.map((user: any) => (
                         <User user={user} key={user.id} />
                     ))}
