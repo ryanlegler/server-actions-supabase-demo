61d958f6d0854324f2da98b68ffcbff908e94bb7
Ryan Legler <ryan.legler@gmail.com>
2023-05-06 08:21:40 -0700

    feature: adds loading state to button

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -1,13 +1,22 @@
 "use client";
+
+import { useTransition } from "react";
+
 export function Button({ onClick }: any) {
+    const [isPending, startTransition] = useTransition();
+
+    const handleClick = async () => {
+        startTransition(() => {});
+        await onClick();
+    };
+
     return (
         <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={async () => {
-                await onClick();
-            }}
+            disabled={isPending}
+            className={`bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded disabled:bg-slate-600`}
+            onClick={handleClick}
         >
-            Do the thing
+            {isPending ? "Saving.." : "Save User"}
         </button>
     );
 }
